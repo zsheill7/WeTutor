2113a7445d620e37ef0348ffbd57a0da2f9e621f
zsheill7 <zsheill7@gmail.com>
2017-01-23 18:37:06 -0800

    Replaced Channel list

--- TextField/AppDelegate.swift
+++ TextField/AppDelegate.swift
@@ -64,7 +64,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                // self.present(viewController, animated: true, completion: nil)
             }
         }
-        /*let userID = FIRAuth.auth()?.currentUser?.uid
+        let userID = FIRAuth.auth()?.currentUser?.uid
         var ref: FIRDatabaseReference!
         
         ref = FIRDatabase.database().reference()
@@ -94,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             // ...
         }) { (error) in
             print(error.localizedDescription)
-        }*/
+        }
         
         
     }
