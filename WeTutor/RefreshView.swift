74a3051716ec08a983a919aa299277f0cb115607
zsheill7 <zsheill7@gmail.com>
2017-04-17 11:25:40 -0700

    Added refresh view xib and swift file

---
+++ WeTutor/RefreshView.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefresh.swift
+//  WeTutor
+//
+//  Created by Zoe on 4/17/17.
+//  Copyright © 2017 CosmicMind. All rights reserved.
+//
+
+import UIKit
+
+class RefreshView: UIView {
+    
+   
+    
+    @IBOutlet var imageView: UIImageView!
+    // and others
+}
