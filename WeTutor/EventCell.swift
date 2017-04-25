9741511d36bda83af68a40113bebfb35c322de13
zsheill7 <zsheill7@gmail.com>
2017-04-25 14:34:50 -0700

    deleted merge message

--- WeTutor/EventCell.swift
+++ WeTutor/EventCell.swift
@@ -1,9 +1,9 @@
 //
 //  eventCell.swift
-//  Add Event
+//  Band App Add Event
 //
 //  Created by Zoe Sheill on 7/12/16.
-//  Copyright © 2016 TokkiTech. All rights reserved.
+//  Copyright © 2016 ClassroomM. All rights reserved.
 //
 
 import UIKit
@@ -23,35 +23,8 @@ class EventCell: UITableViewCell {
     @IBOutlet weak var calendarIcon: UIImageView!
     
     override func awakeFromNib() {
-        var screenBounds = UIScreen.main.bounds
-        
-        let width = screenBounds.size.width
-        let cellHeight = 105
-        let cornerRadius:CGFloat = 5
-        //self.contentView.backgroundColor = UIColor.red
         if let calendarImage = UIImage(named: "calendarIcon") {
             calendarIcon.image = calendarImage
         }
-         let whiteRoundedView : UIView = UIView(frame: CGRect(x: 10, y: 8, width: /*Int(self.frame.size.width - 40)*/Int(width - 20), height: cellHeight))
-        
-        whiteRoundedView.layer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 1.0, 1.0])
-        whiteRoundedView.layer.masksToBounds = false
-        whiteRoundedView.layer.cornerRadius = cornerRadius
-        whiteRoundedView.layer.shadowOffset = CGSize(width: -1, height: 1)
-        whiteRoundedView.layer.shadowOpacity = 0.2
-        
-        //let leftColorView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: cellHeight))
-        //leftColorView.backgroundColor = colors[indexPath.row % 6]
-        
-       // self.isUserInteractionEnabled = true
-        
-        
-        
-        self.alpha = 0
-        UIView.animate(withDuration: 0.3) {
-            self.contentView.alpha = 1
-        }
-        self.addSubview(whiteRoundedView)
-        self.sendSubview(toBack: whiteRoundedView)
     }
 }
