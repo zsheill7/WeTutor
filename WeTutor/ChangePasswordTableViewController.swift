906822f8692e5c7d37c5a8cfda8501f150b63444
zsheill7 <zsheill7@gmail.com>
2017-06-05 10:51:41 -0700

    Deleted background comments

--- WeTutor/ChangePasswordTableViewController.swift
+++ WeTutor/ChangePasswordTableViewController.swift
@@ -22,10 +22,8 @@ class ChangePasswordTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         ref = FIRDatabase.database().reference()
-       // self.view?.backgroundColor = UIColor.backgroundBlue()
     }
     func isValidEmail(testStr:String) -> Bool {
-        // print("validate calendar: \(testStr)")
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
